8db010403216c205eeb283eb9a5c61aa952245a6
Tucker Whitehouse <TuckerWhitehouse@gmail.com>
2016-06-01 19:39:49 -0400

    Jekyll 3

--- README.md
+++ README.md
@@ -1,54 +1,14 @@
-# Pizza Academy [![Build Status][travis-image]][travis-url] [![ISC License][license-image]][license-url]
-
-
-
+# PizzaAcademy.com [![ISC License][license-image]][license-url]
 
 ## Setup
-Install [Bundler](http://bundler.io/) using ```gem install bundler```.
-
-Depdencies defined in the [Gemfile](Gemfile) can then be installed using ```bundle install```.
-
-
-
+```bash
+bundle install --path vendor/bundle
+```
 
 ## Preview
-A live building preview can be started using ```rake site:preview```.
-
-The preview will be avaiable at [http://localhost:4000/](http://localhost:4000/).
-
-
-
-
-## Build
-Builds are done automatically on deploy.
-
-A manual build can be done by running ```rake site:build```.
-
-
-
-
-## Deploy
-Committing to the [master](tree/master) branch will trigger build using [Travis CI](https://travis-ci.org).
-
-If the build is successfull, it will be deployed to the [gh-pages](tree/gh-pages) branch.
-
-A manual deploy can be done by executing the following commands in order:
-* ```rake site:stage```  - fetches the [gh-pages](tree/gh-pages) branch
-* ```rake site:build```  - builds the site
-* ```rake site:deploy``` - commits all changes and pushes to the [gh-pages](tree/gh-pages) branch.
-
-
-
-
---
-If any of the above commands fail because of bundler version issues, prepend the command with ```bundle exec```.
-
-(e.g. ```bundle exec rake site:build```)
-
-
-
-[travis-url]:https://travis-ci.org/absolute413/PizzaAcademy.com
-[travis-image]: https://img.shields.io/travis/absolute413/PizzaAcademy.com.svg?style=flat-square
+```bash
+bundle exec jekyll serve
+```
 
 [license-url]: LICENSE
 [license-image]: https://img.shields.io/badge/license-ISC-000000.svg?style=flat-square
--- _assets/js/default.js
+++ _assets/js/default.js
@@ -1,46 +1,80 @@
----
----
-
 $(function () {
-    // Hours (today)
-    $('.hours-table').parent().find('th').each(function () {
-        var $this = $(this)
-          , label = $this.text()
-          , today = moment().format('dddd');
-        if (label === today) {
-            $this.append(' <small>(today)</small>');
-        }
-    });
+  // Hours (today)
+  $('.hours-table').parent().find('th').each(function () {
+    var $this = $(this)
+    var label = $this.text()
+    var today = moment().format('dddd')
 
-    // Location Map
-    $('.location-map').each(function () {
-        var self = this,
-            Map, Marker, InfoWindow;
+    if (label === today) {
+      $this.append(' <small>(today)</small>')
+    }
+  })
 
-        var MapBuilder = function () {
-            if (Map && Marker && InfoWindow) {return;}
+  // Location Map
+  $('.location-map').each(function () {
+    var self = this
+    var Map
+    var Marker
+    var InfoWindow
 
-            Map = new google.maps.Map(self, {
-                zoom: 14
-              , center: new google.maps.LatLng(42.982000, -70.961426)
-              , scrollwheel: false
-              , disableDefaultUI: true
-            });
+    var MapBuilder = function () {
+      if (Map && Marker && InfoWindow) { return }
 
-            Marker = new google.maps.Marker({
-                map: Map
-              , position: new google.maps.LatLng(42.978998, -70.961426)
-              , title: '{{ site.title }}'
-            });
+      Map = new google.maps.Map(self, {
+        zoom: 14,
+        center: new google.maps.LatLng(42.982000, -70.961426),
+        scrollwheel: false,
+        disableDefaultUI: true
+      });
 
-            InfoWindow = new google.maps.InfoWindow({
-                map: Map
-              , anchor: Marker
-              , content: '{{ site.title }} &middot; {{ site.tagline }}<br>{{ site.description }}<br>159 Front St, Exeter NH, 03833'
-            });
-        };
+      Marker = new google.maps.Marker({
+        map: Map,
+        position: new google.maps.LatLng(42.978998, -70.961426),
+        title: 'Pizza Academy'
+      });
 
-        var $modal = $(this).parents('.modal');
-        $modal.length ? $modal.on('shown.bs.modal', MapBuilder) : MapBuilder();
-    });
-});
+      InfoWindow = new google.maps.InfoWindow({
+        map: Map,
+        anchor: Marker,
+        content: 'Pizza Academy &middot; Subs, Salads & Roast Beef<br>Open 7 days a week, and we deliver all day.<br>159 Front St, Exeter NH, 03833'
+      })
+    }
+
+    var $modal = $(this).parents('.modal')
+    $modal.length ? $modal.on('shown.bs.modal', MapBuilder) : MapBuilder()
+  })
+
+  // Photo Gallery
+  $(function () {
+    var template = _.template([
+      '<div class="col-md-3 col-sm-4">',
+        '<div class="panel panel-default">',
+          '<div class="panel-body">',
+            '<figure class="embed-responsive embed-square" style="background-image: url(<%= picture %>)">',
+              '<h4><%= name %></h4>',
+            '</figure>',
+          '</div>',
+        '</div>',
+      '</div>'
+    ].join(''))
+
+    var filter = _.filter(function (image) { return image.width >= 240 })
+    var sort = _.sortBy('width')
+    var map = _.map(function (photo) {
+      var picture = filter(sort(photo.images))[0].source
+      return template({ name: (photo.name || ''), picture: picture })
+    })
+
+    $('.photo-gallery').each(function () {
+      var $el = $(this)
+      var album = $el.data('album')
+
+      getPhotos(album).then(function (html) { $el.html('<div class="row">'+ html +'</div>') })
+    })
+
+    function getPhotos(album) {
+      var query = { fields: 'name,images', limit: 100, access_token: '1820160931538945|lMcdsMOGR_MH0bHe4GuHlBuhrdo' }
+      return $.ajax('https://graph.facebook.com/' + album + '/photos', { data: query }).then(function (json) { return map(json.data).join('') })
+    }
+  })
+})
