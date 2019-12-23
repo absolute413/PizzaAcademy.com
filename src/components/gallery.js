11b8ef26f62b2cb22e2dbb016f54bd1bfafabea8
Tucker Whitehouse <TuckerWhitehouse@gmail.com>
2019-12-23 13:55:46 -0500

    Use graph.madebyglutard.com vs graph.facebook.com

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 import { map, filter, sortBy } from 'lodash'
 
 function getPhotos(album) {
-  const query = { fields: 'name,images', limit: 100, access_token: '1820160931538945|lMcdsMOGR_MH0bHe4GuHlBuhrdo' }
-  return $.ajax('https://graph.facebook.com/' + album + '/photos', { data: query }).then(json => {
+  const query = { fields: 'name,images', limit: 100 }
+  return $.ajax('https://graph.madebyglutard.com/' + album + '/photos', { data: query }).then(json => {
     return map(json.data, photo => {
       const images = filter(sortBy(photo.images, 'width'), image => image.width >= 240)
       return { name: photo.name || '', picture: images[0].source }
